42102b3699fa09802c7e4678a2bf642dae1ff5cd
Besson Mickaël <mickabesson@hotmail.fr>
2014-08-04 22:42:03 +0200

    Create DB and Sync apps

--- BitBot/settings.py
+++ BitBot/settings.py
@@ -36,6 +36,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'south',
+    'Trade'
 )
 
 MIDDLEWARE_CLASSES = (
--- Trade/models.py
+++ Trade/models.py
@@ -2,6 +2,6 @@ from django.db import models
 
 # Create your models here.
 # -------------------------------------------------------------------------------------------------------------------- #
-# python manage.py schemamigration trade --auto
-# python manage.py migrate trade
+# python manage.py schemamigration Trade --auto
+# python manage.py migrate Trade
 # -------------------------------------------------------------------------------------------------------------------- #
