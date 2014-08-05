2b9e927844585a66a9eb61091f648066028cbc7f
Naze-laptop <jejelorrain@hotmail.fr>
2014-08-05 17:15:42 +0200

    Algorithme modification
    BtcRate --> BtcValue

--- Algorithme.py
+++ Algorithme.py
@@ -6,28 +6,34 @@ import os
 # set up django
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "BitBot.settings")
 
-from Trade.models import BtcValue, Average
+from Trade.models import BtcValue, Average, AlgOption
 
 
 # -------------------------------------------------------------------------------------------------------------------- #
 
-current_rate = BtcValue.objects.filter[:1]
-buy_rate = 0
+current_rate = BtcValue.objects.all()[:1]
+current_rate = current_rate.rate
 
-optionNbr = 0
+lastAverage = Average.objects.all()[:1]
+monthAverage = lastAverage.monthAverage
+dayAverage = lastAverage.dayAveraga
+
+
 options = {0 : Buying,
            1 : Selling,}
 
+options[AlgOption.Status]()
+
 def Buying():
-    if current_rate < Average.object.filter(Average.monthAverage)[:1] & current_rate < Average.objects.filter(Average.dayAverage)[:1]:
-        buy_rate = current_rate
-         buy()
-        optionNbr = 1
+    if current_rate < monthAverage && current_rate < dayAverage:
+        AlgOption.BuyRate = current_rate
+        buy()
+        AlgOption.Status = 1
 
 def Selling():
-    if current_rate >= buy_rate:
+    if current_rate >= AlgOption.BuyRate + 10:
         sell()
-    optionNbr = 0
+    AlgOption.Status = 0
 
 def buy():
 
--- Trade/models.py
+++ Trade/models.py
@@ -7,7 +7,7 @@ from decimal import Decimal
 # -------------------------------------------------------------------------------------------------------------------- #
 
 
-class BtcRate(models.Model):
+class BtcValue(models.Model):
     rate = models.DecimalField(unique=False, blank=False, max_digits=8, decimal_places=2)
     high = models.DecimalField(unique=False, blank=False, max_digits=8, decimal_places=2)
     low = models.DecimalField(unique=False, blank=False, max_digits=8, decimal_places=2)
@@ -30,3 +30,7 @@ class EurUsd(models.Model):
 class BitstampUser(models.Model):
     UserAccount = models.CharField(unique=True, blank=False, max_length=100)
     ApiKey = models.CharField(unique=True, blank=False, max_length=255)
+
+class AlgOption(models.Model):
+    Status  = models.IntegerField(unique=True, blank=False, max_length=2)
+    BuyRate = models.DecimalField(unique=False, blank=False, max_digits=16, decimal_places=8)
