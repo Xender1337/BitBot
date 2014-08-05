ca596d627efe2d3c9c665af963079f982aaf9319
Besson Mickaël <mickabesson@hotmail.fr>
2014-08-05 17:00:48 +0200

    Modify admin and new content for Db

--- Trade/admin.py
+++ Trade/admin.py
@@ -1,21 +1,22 @@
 from django.contrib import admin
-from Trade.models import BtcRate, EurUsd, BitstampUser
-
-# Register your models here.
+from Trade.models import BtcRate, EurUsd, BitstampUser, Average
 
 
 class BtcRateAdmin(admin.ModelAdmin):
     list_display = ('rate', 'high', 'low', 'ask', 'volume', 'date')
-
 admin.site.register(BtcRate, BtcRateAdmin)
 
 
 class EurUsdAdmin(admin.ModelAdmin):
     list_display = ('buy', 'sell', 'date')
-
 admin.site.register(EurUsd, EurUsdAdmin)
+
 
 class BitstampUserAdmin(admin.ModelAdmin):
     list_display = ('UserAccount', 'ApiKey')
-
 admin.site.register(BitstampUser, BitstampUserAdmin)
+
+
+class AverageAdmin(admin.ModelAdmin):
+    list_display = ('monthAverage', 'dayAverage')
+admin.site.register(Average, AverageAdmin)
