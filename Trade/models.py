757e79ebd0ce3c07618b112253184de67ca5052d
Besson Mickaël <mickabesson@hotmail.fr>
2014-08-05 17:15:33 +0200

    Update DB

--- Trade/models.py
+++ Trade/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from decimal import Decimal
 
-# Create your models here.
 # -------------------------------------------------------------------------------------------------------------------- #
 # python manage.py schemamigration Trade --auto
 # python manage.py migrate Trade
@@ -15,6 +14,7 @@ class BtcRate(models.Model):
     ask = models.DecimalField(unique=False, blank=False, max_digits=8, decimal_places=2)
     volume = models.DecimalField(unique=False, blank=False, max_digits=16, decimal_places=8)
     date = models.DateTimeField(unique=False, blank=False, auto_now_add=True)
+
 
 class Average(models.Model):
     monthAverage = models.DecimalField(unique=False, blank=False, max_digits=16, decimal_places=8)
