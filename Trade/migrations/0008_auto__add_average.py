e13f6db3435b6391d515bd98600ac8ffff08d45b
Besson Mickaël <mickabesson@hotmail.fr>
2014-08-05 14:46:29 +0200

    New Migration with average models.

---
+++ Trade/migrations/0008_auto__add_average.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+from south.utils import datetime_utils as datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding model 'Average'
+        db.create_table(u'Trade_average', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('monthAverage', self.gf('django.db.models.fields.DecimalField')(max_digits=16, decimal_places=8)),
+            ('dayAverage', self.gf('django.db.models.fields.DecimalField')(max_digits=16, decimal_places=8)),
+        ))
+        db.send_create_signal(u'Trade', ['Average'])
+
+
+    def backwards(self, orm):
+        # Deleting model 'Average'
+        db.delete_table(u'Trade_average')
+
+
+    models = {
+        u'Trade.average': {
+            'Meta': {'object_name': 'Average'},
+            'dayAverage': ('django.db.models.fields.DecimalField', [], {'max_digits': '16', 'decimal_places': '8'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'monthAverage': ('django.db.models.fields.DecimalField', [], {'max_digits': '16', 'decimal_places': '8'})
+        },
+        u'Trade.bitstampuser': {
+            'ApiKey': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '255'}),
+            'Meta': {'object_name': 'BitstampUser'},
+            'UserAccount': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '100'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'})
+        },
+        u'Trade.btcrate': {
+            'Meta': {'object_name': 'BtcRate'},
+            'ask': ('django.db.models.fields.DecimalField', [], {'max_digits': '8', 'decimal_places': '2'}),
+            'date': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'high': ('django.db.models.fields.DecimalField', [], {'max_digits': '8', 'decimal_places': '2'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'low': ('django.db.models.fields.DecimalField', [], {'max_digits': '8', 'decimal_places': '2'}),
+            'rate': ('django.db.models.fields.DecimalField', [], {'max_digits': '8', 'decimal_places': '2'}),
+            'volume': ('django.db.models.fields.DecimalField', [], {'max_digits': '16', 'decimal_places': '8'})
+        },
+        u'Trade.eurusd': {
+            'Meta': {'object_name': 'EurUsd'},
+            'buy': ('django.db.models.fields.DecimalField', [], {'max_digits': '5', 'decimal_places': '4'}),
+            'date': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'sell': ('django.db.models.fields.DecimalField', [], {'max_digits': '5', 'decimal_places': '4'})
+        }
+    }
+
+    complete_apps = ['Trade']
